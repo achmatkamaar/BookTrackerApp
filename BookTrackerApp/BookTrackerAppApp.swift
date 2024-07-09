051339e1e2aa9dd46e92162130b7db55b70ea853
Achmat Kamaar <akamaar166@gmail.com>
2024-07-09 07:51:50 +0200

    Initial Commit

---
+++ BookTrackerApp/BookTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTrackerAppApp.swift
+//  BookTrackerApp
+//
+//  Created by Achmat Kamaar on 2024/07/09.
+//
+
+import SwiftUI
+
+@main
+struct BookTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
